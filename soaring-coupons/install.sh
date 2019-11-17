a99dede332da65ddce837d4015b7cbe12f7bdbde
Andrey Lebedev <andrey@lebedev.lt>
2019-11-17 18:21:25 +0200

    Configure logging for soaring-coupons

--- soaring-coupons/install.sh
+++ soaring-coupons/install.sh
@@ -7,3 +7,8 @@ dokku proxy:ports-set $APP http:80:8080
 dokku postgres:create $APP-db
 dokku postgres:link $APP-db $APP
 dokku nginx:build-config $APP
+
+dokku docker-options:add soaring-coupons deploy,run "--log-driver=fluentd"
+dokku docker-options:add soaring-coupons deploy,run "--log-opt tag=docker.soaring-coupons"
+dokku docker-options:add soaring-coupons deploy,run "--log-opt fluentd-async-connect=true"
+dokku docker-options:add soaring-coupons deploy,run "--log-opt fluentd-sub-second-precision=true"
